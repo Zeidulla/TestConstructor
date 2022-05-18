362bd6b7ce94713a34b75f667c791e8d0232009b
Zeidulla <denakannem@gmail.com>
2022-05-18 23:31:02 +0400

    проект maven. pom.xml

---
+++ src/main/java/com/testsystem/TestConstructor/TestConstructorApplication.java
@@ -0,0 +1,14 @@
+package com.testsystem.TestConstructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConstructorApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/testsystem/TestConstructor/TestConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testsystem.TestConstructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
