fc0c393f56fbe03ee93818137e456bbc0116faed
JaxHimer <105057494+JaxHimer@users.noreply.github.com>
2022-05-24 21:59:42 +0300

    UserService и доработка

--- src/main/java/com/testsystem/TestConstructor/models/Test.java
+++ src/main/java/com/testsystem/TestConstructor/models/Test.java
@@ -11,64 +11,85 @@ public class Test { /* сделаю пометку */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
+
     private String description;
-    private boolean publish; /* сделаю пометку */
+
+    private boolean publish;
+
     @ManyToOne
     private User user;
+
     @OneToMany(cascade = CascadeType.ALL)
-    private List <TypePatternQuestions.Question> question; /* нужно проверить! */
+    private List<Question> question;
+
     @OneToMany(cascade = CascadeType.ALL)
-    private List <Result> result;
-    public Test (User user, String name, String description){
+    private List<Result> result;
+
+    public Test(User user,String name, String description) {
         this.user = user;
         this.name = name;
         this.description = description;
-        this.publish = false;/* сделаю пометку */
+        this.publish=false;
     }
     public Test(){}
-    public Long getId(){/* сделаю пометку */
+
+    public Long getId() {
         return id;
     }
-    public void setId(Long id) {/* сделаю пометку */
+
+    public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public List<TypePatternQuestions.Question> getQuestion() {/* нужно проверить! */
+
+    public List<Question> getQuestion() {
         return question;
     }
-    public void setQuestion(List<TypePatternQuestions.Question> question) {/* нужно проверить! */
+
+    public void setQuestion(List<Question> question) {
         this.question = question;
     }
-    public void addQuest (TypePatternQuestions.Question question){/* нужно проверить! */
-        this.question.add(question);
-    }
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    public void  setDescription(String description) {
+
+    public void setDescription(String description) {
         this.description = description;
     }
+
     public User getUser() {
         return user;
     }
-    public void  setUser(User user) {
+
+    public void setUser(User user) {
         this.user = user;
     }
+    public void addQuest(Question question){
+        this.question.add(question);
+    }
+
     public boolean isPublish() {
         return publish;
     }
+
     public void setPublish(boolean publish) {
         this.publish = publish;
     }
-    public List<Result> getResult(){
+
+    public List<Result> getResult() {
         return result;
     }
+
     public void setResult(List<Result> result) {
         this.result = result;
     }
