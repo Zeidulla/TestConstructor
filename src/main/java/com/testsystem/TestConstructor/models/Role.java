631c6a2712ec7e827e144481290c1a95262441bc
Zeidulla <denakannem@gmail.com>
2022-05-24 19:28:32 +0400

    Добавление регистрации администратора

--- src/main/java/com/testsystem/TestConstructor/models/Role.java
+++ src/main/java/com/testsystem/TestConstructor/models/Role.java
@@ -1,5 +1,7 @@
 package com.testsystem.TestConstructor.models;
 
+//https://habr.com/ru/post/482552/
+
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -8,8 +10,8 @@ import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
-public class Role implements GrantedAuthority {
-    @Id
+public class Role implements GrantedAuthority { //класс Role реализовывает интерфейс GrantedAuthority,
+    @Id                                        //в котором необходимо переопределить только один метод getAuthority() (возвращает имя роли)
     private Long id;
 
     private String name;
@@ -18,25 +20,31 @@ public class Role implements GrantedAuthority {
     private List<User> users;
 
     public Role(){}
-    public  Role(String role) {/* сделаю пометку */
+    public  Role(String role){
         if(role.equals("user")==true) {
             id = Long.valueOf("1");
             name = "ROLE_USER";
-        } else{
+        } else if(role.equals("tester")==true){
             id = Long.valueOf("2");
             name = "ROLE_TESTER";
+        } else {
+            id = Long.valueOf("3");
+            name = "ROLE_ADMIN";
         }
     }
-    public Long getId() {
+    public Long getId() {  //публичный конструктор: только id
         return id;
     }
-    public void setId(Long id) {
+
+    public void setId(Long id) {  //публичный конструктор: только id
         this.id = id;
     }
-    public String getName() {
+
+    public String getName() { //публичный конструктор: только id и name
         return name;
     }
-    public void setName(String name) {
+
+    public void setName(String name) { //публичный конструктор: только id и name
         this.name = name;
     }
 
