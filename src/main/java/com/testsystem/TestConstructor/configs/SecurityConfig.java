1a011ac6acbab7a71aab87e6d4e3880950c9db4a
Zeidulla <denakannem@gmail.com>
2022-05-27 01:06:37 +0400

    SecurityConfig

--- src/main/java/com/testsystem/TestConstructor/configs/SecurityConfig.java
+++ src/main/java/com/testsystem/TestConstructor/configs/SecurityConfig.java
@@ -1,32 +1,36 @@
 package com.testsystem.TestConstructor.configs;
 
+//https://sysout.ru/nastrojka-jdbc-autentifikatsii-v-spring-security/
 
+import com.testsystem.TestConstructor.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+@Configuration  ////эта аннотация используется для классов, которые определяют bean-компоненты
+@EnableWebSecurity //@EnableWebSecurity включает поддержку web security и обеспечивает интеграцию со Spring MVC. сonfigure(HttpSecurity) определяет какой URL нужно защищать, а какой не надо(по умолчанию все защищено)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserService userService;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/register", "/confirmRegistration/**", "/recoverPass", "/recoverPassword/**").anonymous()
-                .antMatchers("/*.css", "/*.js").permitAll()
-                .antMatchers("/test/**").hasRole("TESTER")
-                .antMatchers("/admin/**", "/delete-user/**").hasRole("ADMIN")
+    @Override //За авторизацию отвечает метод configure(HttpSecurity http):
+    protected void configure(HttpSecurity http) throws Exception { //для авторизации
+        http.authorizeRequests()
+                .antMatchers("/register","/confirmRegistration/**","/recoverPass","/recoverPassword/**").anonymous()
+                .antMatchers("/*.css","/*.js").permitAll()  //    / для всех пользователей (в том числе не аутентифицированных)
+                .antMatchers("/test/**").hasRole("TESTER")  //  /test для пользователей с ролью TESTER
+                .antMatchers("/admin/**", "/delete-user/**").hasRole("ADMIN") //  /admin  для пользователей с ролью ADMIN
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -36,4 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
+
+    @Autowired //За настройку аутентификации отвечает метод configure(AuthenticationManagerBuilder auth)
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { //для аутентификации
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
 }
